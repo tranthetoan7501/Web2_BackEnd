729e2d82d1480b1486e5814916a557ae87a7ad78
Nguyen Tran Thien Toan <thientoancubi@gmail.com>
2022-11-30 22:20:36 +0700

    log database url

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(passport.session());
 
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://kahit-lemon.vercel.app',
     methods: 'GET,POST,PUT,DELETE',
     credentials: true,
   })
