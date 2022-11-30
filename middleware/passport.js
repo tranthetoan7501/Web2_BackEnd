6d4ae95b6985643ff3022f237da25f075dd09072
Nguyen Tran Thien Toan <thientoancubi@gmail.com>
2022-11-30 21:15:52 +0700

    update config

--- middleware/passport.js
+++ middleware/passport.js
@@ -34,7 +34,7 @@ passport.use(
   new JwtStrategy(
     {
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken('Authorization'),
-      secretOrKey: 'secret key',
+      secretOrKey: "secret key",
     },
     async function (jwt_payload, done) {
       try {
@@ -55,37 +55,15 @@ passport.use(
   )
 );
 
-// passport.use(
-//   new GoogleStrategy(
-//     {
-//       clientID:
-//         '1043053836697-q5sku2jln8bohhqm2oqu25auha4als3u.apps.googleusercontent.com', //GOOGLE_CLIENT_ID,
-//       clientSecret: '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',
-//       callbackURL: 'http://localhost:5001/user/google/callback',
-//     },
-//     function (accessToken, refreshToken, profile, callback) {
-//       // User.findOrCreate({ googleId: profile.id }, function (err, user) {
-//       //   return callback(err, user);
-//       // });
-//       return callback(null, profile);
-//     }
-//   )
-// );
-
 passport.use(
   new GoogleStrategy(
     {
       clientID:
         '1043053836697-q5sku2jln8bohhqm2oqu25auha4als3u.apps.googleusercontent.com', //GOOGLE_CLIENT_ID,
       clientSecret: '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',
-      callbackURL: 'http://localhost:5001/api/user/auth/google/callback',
+      callbackURL: '/api/user/auth/google/callback',
       scope: [ 'profile' ],
     }, async (accessToken, refreshToken, profile, done) => {
-      console.log("\n\naccessToken: ", accessToken);
-      console.log("\n\nprofile", JSON.stringify(profile))
-
-      console.log("\n\n")
-
       // Check if google profile exist.
       if (profile.id) {
         const existingUser = await userService.findUserByGoogleID(profile.id)
